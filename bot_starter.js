1cd756b3dbf5f73c1e844257e3a206a53558fb7b
saschalalala <sascha.zantis@gmail.com>
2014-09-19 13:24:40 +0200

    Cleanup

--- bot_starter.js
+++ bot_starter.js
@@ -84,7 +84,7 @@ function checkShell() {
 function testQuery() {
   var key = '"niemals"';
   var query = 'SELECT fact FROM factoids INNER JOIN keys on factoids.id = keys.factoid_id WHERE keys.key LIKE' + key + ';';
-  db.each(query, function(err, value) {
+  db.each(query, function (err, value) {
     var answer = value.fact;
     console.log(answer);
   });
@@ -102,7 +102,7 @@ function replaceText(from, to, messageLog, text) {
     global_replacement = 'g';
   }
   //just continue if the regexes are well formed
-  if (splitted.length === 4 && typeof(lastMessage.text) != 'undefined') {
+  if (splitted.length === 4 && typeof (lastMessage.text) != 'undefined') {
     try {
       var a = new RegExp(splitted[1], global_replacement);
       var b = splitted[2];
@@ -127,7 +127,7 @@ function queryFact(to, text) {
   }
   var query = 'SELECT fact FROM factoids INNER JOIN keys on factoids.id = keys.factoid_id WHERE keys.key LIKE' + key + ';';
   if (db.open) {
-    db.each(query, function(err, value) {
+    db.each(query, function (err, value) {
       var answer = value.fact;
       if (userAdressed) {
         answer = username + ": " + answer;
@@ -143,7 +143,7 @@ function getPageTitle(to, httpLink) {
     url: String(httpLink),
     method: 'GET'
   };
-  request(requestOptions, function(error, response, html) {
+  request(requestOptions, function (error, response, html) {
     if (!error) {
       var page = cheerio.load(html);
       var title = page('title').text();
@@ -156,47 +156,46 @@ function getPageTitle(to, httpLink) {
 }
 
 // Listen for joins
-bot.addListener("join", function(channel, who) {
+bot.addListener("join", function (channel, who) {
   // Welcome them in!
 });
 
-bot.addListener("message", function(from, to, text, message) {
-    //Log all messages
-    messageLog.push(new Message(from, text, to));
-    //Get Regex
-    //Regex for text replacement
-    var regexStart = new RegExp('^s/', 'g');
-    //Regex for sql query
-    var queryStart = new RegExp('^!', 'g');
-    //Simple Regex (http in string)
-    var httpInString = new RegExp('http', 'g')
-      //Regex for http links
-    var fullHttpLink = new RegExp(/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i); //Thanks to https://gist.github.com/dperini/729294 and https://mathiasbynens.be/demo/url-regex
-    //Regex for letting the bot talk
-    var sayStart = new RegExp('^SAY', 'g');
-    //Regex bot-Name
-    var reBotName = new RegExp('^' + config.botName, 'g');
+bot.addListener("message", function (from, to, text, message) {
+  //Log all messages
+  messageLog.push(new Message(from, text, to));
+  //Get Regex
+  //Regex for text replacement
+  var regexStart = new RegExp('^s/', 'g');
+  //Regex for sql query
+  var queryStart = new RegExp('^!', 'g');
+  //Simple Regex (http in string)
+  var httpInString = new RegExp('http', 'g')
+  //Regex for http links
+  var fullHttpLink = new RegExp(/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/i); //Thanks to https://gist.github.com/dperini/729294 and https://mathiasbynens.be/demo/url-regex
+  //Regex for letting the bot talk
+  var sayStart = new RegExp('^SAY', 'g');
+  //Regex bot-Name
+  var reBotName = new RegExp('^' + config.botName, 'g');
 
-    if (reBotName.test(text)) {
-      bot.say(to, 'I\'m a bot.');
-    }
-    //Replace sed style
-    //s/a/b/ <- no other syntax
-    else if (regexStart.test(text)) {
-      replaceText(from, to, messageLog, text);
-    } else if (queryStart.test(text)) {
-      queryFact(to, text);
-    } else if (httpInString.test(text)) {
-        //get the page into a html obj
-        var splitted = text.split(' ');
-        splitted.forEach(function(element) {
-          var link = fullHttpLink.exec(element);
-          if (link != null) {
-            getPageTitle(to, link);
-          }
-        });
+  if (reBotName.test(text)) {
+    bot.say(to, 'I\'m a bot.');
+  }
+  //Replace sed style
+  //s/a/b/ <- no other syntax
+  else if (regexStart.test(text)) {
+    replaceText(from, to, messageLog, text);
+  } else if (queryStart.test(text)) {
+    queryFact(to, text);
+  } else if (httpInString.test(text)) {
+    //get the page into a html obj
+    var splitted = text.split(' ');
+    splitted.forEach(function (element) {
+      var link = fullHttpLink.exec(element);
+      if (link != null) {
+        getPageTitle(to, link);
       }
-    } else if (to === config.controlchannel && from === config.admin_user) {
+    });
+  } else if (to === config.controlchannel && from === config.admin_user) {
     if (text === 'UPDATE_DB') {
       console.log("Not activated, need sqlite2, not available at uperspace by default");
       return 0;
